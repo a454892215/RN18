f6b9355291e3979159cc475ba3f00a44a4ae1abb
Henry <llpph866@gmail.com>
2024-08-31 15:11:49 +0700

    升级？代码提示

--- run-android.sh
+++ run-android.sh
@@ -1,4 +1,5 @@
 # 指定打包服务端口号 以 debug 模式运行编译安装运行app
+nvm use 18
 yarn react-native run-android --port=8082
 # 指定打包服务端口号 以 release模式运行编译安装运行app
 # npx react-native run-android --variant=release --port=8082
--- start-server.sh
+++ start-server.sh
@@ -2,6 +2,7 @@
 # yarn start -- --reset-cache
 watchman watch-del '../RN18'
 watchman watch-project '../RN18'
+nvm use 18
 yarn install
 yarn start -- --reset-cache --port=8082
 
