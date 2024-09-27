8a5cae20c7169dfaec40157d7638027e967c6554
Henry <llpph866@gmail.com>
2024-09-27 15:04:14 +0700

    配置WebStorm Run窗口启动服务

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,11 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: ['@babel/plugin-transform-flow-strip-types'],
+  env: {
+    development: {
+      plugins: require('fs').existsSync('./source-location-logger.js')
+        ? ['./source-location-logger.js']
+        : [],
+    },
+  },
 };
--- source-location-logger.js
+++ source-location-logger.js
@@ -1,4 +1,5 @@
-// 使用onPressIn处理 源码定位Log
+// 使用说明，如果组件是Image组件，给其添加的onPress会无效
+// 功能说明：点击文字组件，或者已经添加onPress属性的组件，会自动打印出改组件源码位置，只在开发坏境使用
 module.exports = function ({types: t}) {
   return {
     visitor: {
@@ -10,68 +11,53 @@ module.exports = function ({types: t}) {
         if (!filePath.includes('node_modules')) {
           const fileName = filePath.split('/').pop(); // 仅获取文件名
           const lineNumber = path.node.loc.start.line; // 获取行号
-
-          // 处理按下事件日志打印
+          // 处理点击事件日志打印
           const logStatement = t.expressionStatement(
             t.callExpression(
               t.memberExpression(t.identifier('console'), t.identifier('log')),
               [
                 t.stringLiteral(
-                  `========LLpp=============>: at ${fileName}:${lineNumber}`,
+                  `at ========LLpp=============>: (${fileName}:${lineNumber})`,
                 ),
               ],
             ),
           );
 
-          // 支持 onPressIn 的组件名列表
-          const supportedComponents = [
-            'TouchableOpacity',
-            'TouchableHighlight',
-            'TouchableWithoutFeedback',
-            'Pressable',
-          ];
+          // 检查是否有 onPress 属性
+          const existingOnPress = path.node.attributes.find(
+            attr => attr.name && attr.name.name === 'onPress',
+          );
 
-          // 获取组件的名称
-          const componentName = path.node.name.name;
+          if (existingOnPress) {
+            // 如果已有 onPress 事件，将日志逻辑注入到现有的 onPress 中
+            const existingOnPressValue = existingOnPress.value.expression;
 
-          // 检查是否是支持 onPressIn 的组件
-          if (supportedComponents.includes(componentName)) {
-            // 检查是否有 onPressIn 属性
-            const existingOnPressIn = path.node.attributes.find(
-              attr => attr.name && attr.name.name === 'onPressIn',
+            // 创建新的函数，先执行日志打印，再执行原本的 onPress 逻辑
+            const newOnPress = t.arrowFunctionExpression(
+              [],
+              t.blockStatement([
+                logStatement,
+                t.expressionStatement(
+                  t.callExpression(existingOnPressValue, []),
+                ),
+              ]),
             );
 
-            if (existingOnPressIn) {
-              // 如果已有 onPressIn 事件，将日志逻辑注入到现有的 onPressIn 中
-              const existingOnPressInValue = existingOnPressIn.value.expression;
-
-              // 创建新的函数，先执行日志打印，再执行原本的 onPressIn 逻辑
-              const newOnPressIn = t.arrowFunctionExpression(
-                [],
-                t.blockStatement([
-                  logStatement,
-                  t.expressionStatement(
-                    t.callExpression(existingOnPressInValue, []),
-                  ),
-                ]),
-              );
-
-              // 更新 onPressIn 属性
-              existingOnPressIn.value = t.jsxExpressionContainer(newOnPressIn);
-            } else {
-              // 如果没有 onPressIn，添加新的 onPressIn 属性
-              path.node.attributes.push(
-                t.jsxAttribute(
-                  t.jsxIdentifier('onPressIn'),
-                  t.jsxExpressionContainer(
-                    t.arrowFunctionExpression(
-                      [],
-                      t.blockStatement([logStatement]),
-                    ),
+            // 更新 onPress 属性
+            existingOnPress.value = t.jsxExpressionContainer(newOnPress);
+          } else {
+            // 如果没有 onPress，添加新的 onPress 属性
+            path.node.attributes.push(
+              t.jsxAttribute(
+                t.jsxIdentifier('onPress'),
+                t.jsxExpressionContainer(
+                  t.arrowFunctionExpression(
+                    [],
+                    t.blockStatement([logStatement]),
                   ),
                 ),
-              );
-            }
+              ),
+            );
           }
         }
       },
