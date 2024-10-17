fd0ec3455aa8f82cb597a1faa8de39b6ec75f70b
Henry <llpph866@gmail.com>
2024-10-17 20:48:11 +0700

    事件版本优化

--- app/com/rn18/CustomDrawerTest.js
+++ app/com/rn18/CustomDrawerTest.js
@@ -1,9 +1,43 @@
 // CustomDrawer.js
-import React, {useRef, useState} from 'react';
+import {EventEmitter} from 'events';
+import React, {useEffect, useRef, useState} from 'react';
 import {Animated, View, Text, TouchableOpacity} from 'react-native';
 
+const eventBus = new EventEmitter();
+const toggle2MenuEvent = 'openMenuEvent';
+
 const CustomDrawerTest = () => {
-  return <CustomDrawer />;
+  return (
+    <View
+      style={{
+        flexDirection: 'column',
+        flex: 1,
+        backgroundColor: '#ff0000',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <CustomDrawer />
+      {/*悬浮按钮*/}
+      <TouchableOpacity
+        style={{
+          position: 'absolute',
+          right: 30,
+          bottom: 30,
+          width: 60,
+          height: 60,
+          borderRadius: 30,
+          backgroundColor: '#6200ee',
+          justifyContent: 'center',
+          alignItems: 'center',
+          elevation: 5,
+        }}
+        onPress={() => {
+          eventBus.emit(toggle2MenuEvent);
+        }}>
+        <Text style={{fontSize: 16, color: '#ffffff'}}>开关</Text>
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 // eslint-disable-next-line no-unused-vars
@@ -29,6 +63,18 @@ const CustomDrawer = () => {
     }
   };
 
+  useEffect(() => {
+    // 监听事件
+    const toggleMenuListener = () => {
+      console.debug('===received toggleMenuListener====isOpen：' + isOpen);
+      toggleMenu();
+    };
+    eventBus.on(toggle2MenuEvent, toggleMenuListener);
+    return () => {
+      eventBus.off(toggle2MenuEvent, toggleMenuListener); // 清除监听
+    };
+  }, [isOpen]);
+
   const translateX = animation.interpolate({
     inputRange: [0, 1],
     outputRange: [menuWidth, 0], // 右边菜单隐藏
@@ -36,44 +82,24 @@ const CustomDrawer = () => {
   });
 
   return (
-    <View style={{flex: 1}}>
-      <Animated.View
-        style={[
-          {
-            position: 'absolute',
-            right: 0,
-            top: 0,
-            bottom: 0,
-            width: menuWidth,
-            height: 180,
-            backgroundColor: '#335c29',
-            justifyContent: 'center',
-            alignItems: 'center',
-          },
-          {transform: [{translateX}]},
-        ]}>
-        {/* 菜单内容 */}
-        <Text>菜单内容</Text>
-      </Animated.View>
-
-      {/*悬浮按钮*/}
-      <TouchableOpacity
-        style={{
+    <Animated.View
+      style={[
+        {
           position: 'absolute',
-          right: 30,
-          bottom: 30,
-          width: 60,
-          height: 60,
-          borderRadius: 30,
-          backgroundColor: '#6200ee',
+          right: 0,
+          top: 0,
+          bottom: 0,
+          width: menuWidth,
+          height: 180,
+          backgroundColor: '#335c29',
           justifyContent: 'center',
           alignItems: 'center',
-          elevation: 5,
-        }}
-        onPress={toggleMenu}>
-        <Text style={{fontSize: 16, color: '#ffffff'}}>开关</Text>
-      </TouchableOpacity>
-    </View>
+        },
+        {transform: [{translateX}]},
+      ]}>
+      {/* 菜单内容 */}
+      <Text>菜单内容</Text>
+    </Animated.View>
   );
 };
 
